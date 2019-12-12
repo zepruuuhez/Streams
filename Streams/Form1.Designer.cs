59e68f2bd50933141a29b4d6615752f3920f1bf9
zepruuuhez <57395202+zepruuuhez@users.noreply.github.com>
2019-12-12 18:46:50 +0700

    Initial commit

---
+++ Streams/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Streams
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Запись";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(286, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Чтение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(46, 177);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(435, 191);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(338, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(556, 380);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Streams/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Streams
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Fill (int[,] mas, int row, int column)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    richTextBox1.Text += mas[i, j] + " ";
+                }
+                richTextBox1.Text += "\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("Files/file2.txt",true); // вызываем класс, даем имя ; пишем относительный путь
+            string str = textBox1.Text; 
+            sw.WriteLine(str);
+            sw.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader("Files/file2.txt",Encoding.Default))
+            {
+                richTextBox1.Text = "";
+                // richTextBox1.Text = sr.ReadLine(); считали 1 строку
+                // richTextBox1.Text += "\n" + sr.ReadLine(); считали 2 строку
+                //richTextBox1.Text = sr.ReadToEnd(); считали все
+
+                while (!sr.EndOfStream) // отриц. знак значит - пока не..
+                {
+                    richTextBox1.Text += sr.ReadLine() + "\n";
+                }
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int Row = 0;
+            richTextBox1.Text = "";
+            int[,] arr = new int[5, 10];
+
+            StreamReader sr = new StreamReader("Files/matrica.txt");
+
+            while (!sr.EndOfStream )
+            {
+                string[] NumbersInString = sr.ReadLine().Split(' ') ; // по сути - new int 10, это делает split
+                for (int i = 0; i < 10; i++)
+                {
+                    arr[Row, i] = Convert.ToInt32(NumbersInString[i]);
+                }
+                Row++;
+            }
+
+            sr.Close();
+            Fill(arr,5,10);
+        }
+    }
+}
